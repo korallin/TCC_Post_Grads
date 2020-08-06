6f08a6883818ce776f4af017c0ffe0d21eedf5c0
Kallil12 <kallilaraujo@gmail.com>
2020-08-06 15:30:58 -0300

    testando multiplos inputs

---
+++ 01 - Prototipo/04_criando_painel_2.py
@@ -0,0 +1,77 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+import pandas as pd
+from numpy import random
+import plotly.graph_objs as go
+import pandas_datareader.data as web
+from datetime import datetime
+import numpy as np
+import os
+
+app = dash.Dash()
+
+dados_varas = pd.read_csv('../dados-pje-MPF.csv',dtype='unicode')
+
+dados_varas['ano_primeira_dist'] = pd.DatetimeIndex(dados_varas['Data Primeira Distribuição']).year
+dados_varas['mes_primeira_dist'] = pd.DatetimeIndex(dados_varas['Data Primeira Distribuição']).month
+
+'''
+Index(['Unnamed: 0', '%ID_PROCESSO_TRF', 'Sistema', 'Número Processo',
+       'Status do Processo', 'Data Última Distribuição', 'Assunto Código',
+       'Assunto', 'Data Primeira Distribuição', 'Classe Judicial',
+       'Órgão Julgador', 'Parte Polo Ativo?', 'Parte Polo Passivo?',
+       'Parte Outros Participantes?', 'Parte Documento', 'Parte Descrição',
+       '%ID_PESSOA_REU', 'Data Trânsito Julgado', 'Número Tempo em Anos'],
+'''
+
+opcoes_ano = []
+for ano in dados_varas['ano_primeira_dist'].unique():
+    opcoes_ano.append({'label':str(ano),'value':ano})
+
+opcoes_vara = []
+for vara in dados_varas['Órgão Julgador'].unique():
+    opcoes_vara.append({'label':str(vara),'value':vara})
+
+app.layout = html.Div([
+             html.H1('Painel de visualização dos assuntos mês a mês'),
+             html.Div([html.H3('Selecione uma vara: ', style={'paddingRight':'30px'}),
+             dcc.Dropdown(id='escolhe-vara',options=opcoes_vara,value='None')],
+             style={'display':'inline-block','verticalAlign':'top','width':'30%'}),
+
+             html.Div([html.H3('Selecione o ano a ser visualizado: ',style={'paddingRight':'30px'}),
+             dcc.Dropdown(id='escolhe-ano',options=opcoes_ano,value='None')],
+             style={'display':'inline-block','verticalAlign':'top','width':'30%'}),
+
+             html.Div([html.Button(id='botao-enviar',n_clicks=0,children='submit',
+                                   style={'fonteSize':24,'marginLeft':'30px'})],
+             style={'display':'inline-block'}),
+             dcc.Graph(id='grafico')
+])
+
+@app.callback(dash.dependencies.Output('grafico','figure'),
+             [dash.dependencies.Input('botao-enviar','n_clicks')],
+             [dash.dependencies.State('escolhe-vara','value')],
+             [dash.dependencies.State('escolhe-ano','value')])
+def update_figure(n_clicks,vara_selecionada,ano_escolhido):
+    #dados apenas para seleção do menu dropdown
+    vara_escolhida = dados_varas[dados_varas['Órgão Julgador']==vara_selecionada]
+    vara_escolhida = vara_escolhida[vara_escolhida['ano_primeira_dist']==ano_escolhido]
+    traces = []
+
+    for assunto_processo in vara_escolhida['Assunto'].unique():
+        vara_filtrada = vara_escolhida[vara_escolhida['Assunto']==assunto_processo]
+        traces.append(go.Bar(
+               x=vara_filtrada['mes_primeira_dist'],
+               y=vara_filtrada['Assunto'],
+         ))
+
+    return {'data':traces,
+            'layout':go.Layout(title='Assunto dos processos por Vara',
+                               xaxis = {'title':'Data da distrubuição'},
+                               yaxis = {'title':'Assunto'})}
+
+
+if __name__ == '__main__':
+    app.run_server()
---
+++ 01 - Prototipo/05_criando_painel_3.py
@@ -0,0 +1,46 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+import pandas as pd
+
+df = pd.read_csv('../mpg.csv')
+
+app = dash.Dash()
+features = df.columns
+
+app.layout = html.Div([
+             html.Div([
+                 dcc.Dropdown(id='xaxis',
+                              options=[{'label':i,'value':i}for i in features],
+                              value='displacement')
+             ],style={'width':'48%','display':'inline-block'}),
+             html.Div([
+                 dcc.Dropdown(id='yaxis',
+                              options=[{'label':i,'value':i}for i in features],
+                              value='mpg')
+             ],style={'width':'48%','display':'inline-block'}),
+             dcc.Graph(id='feature-graphic')
+
+],style={'padding':10})
+
+@app.callback(Output('feature-graphic','figure'),
+             [Input('xaxis','value'),
+              Input('yaxis','value')])
+def update_graph(xaxis_name,yaxis_name):
+    return {'data':[go.Scatter(x=df[xaxis_name],
+                               y=df[yaxis_name],
+                               text=df['name'],
+                               mode='markers',
+                               marker={'size':15,
+                                       'opacity':0.5,
+                                       'line':{'width':0.5,'color':'white'}})],
+
+            'layout':go.Layout(title='Painel para o dataset MPG',
+                               xaxis={'title':xaxis_name},
+                               yaxis={'title':yaxis_name},
+                               hovermode='closest')}
+
+if __name__ == "__main__":
+    app.run_server()
