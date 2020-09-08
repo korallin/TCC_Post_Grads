a5cd05132a3e1724316f25c5e5d92a0f4848e411
Kallil12 <kallilaraujo@gmail.com>
2020-09-08 20:26:26 -0300

    nova tabela de visualização no painel

--- 01 - Prototipo/26_painel_medium.py
+++ 01 - Prototipo/26_painel_medium.py
@@ -139,7 +139,7 @@ content_first_row = dbc.Row([
 
 content_second_row = dbc.Row([
     dbc.Col(
-        html.Div([dcc.Graph(id='grafico_1')],style={"width": "100%", "display": "inline-block"}),
+        html.Div([dcc.Graph(id='grafico_1',responsive=True)],style={"border":"2px black solid"}),
     )])
 
 '''
@@ -170,6 +170,16 @@ content_fourth_row = dbc.Row([
         ,md=12)
 ])
 
+content_sixth_row = dbc.Row([
+        dbc.Col(
+        html.Div([
+        html.Div(id="table3",children='tabela_atualizada')
+        #html.Div(id='submit-button-2', children='Ver tabela')
+])
+        ,md=12)
+])
+
+
 content_fifth_row = dbc.Row([
         dbc.Col(
         html.Div([
@@ -193,7 +203,9 @@ content = html.Div(
         html.Hr(),
         content_fourth_row,
         html.Hr(),
-        content_fifth_row
+        content_fifth_row,
+        html.Hr(),
+        content_sixth_row
 
     ],
     style=CONTENT_STYLE
@@ -209,58 +221,42 @@ server = app.server
              [Input('escolhe-vara','value'),
               Input('escolhe-ano','value')])
 def update_figure(vara_selecionada,ano_escolhido):
+    #dados apenas para seleção do menu dropdown
+    vara_escolhida = dados_varas[dados_varas['Órgão Julgador']==vara_selecionada]
+    vara_escolhida = vara_escolhida[vara_escolhida['ano_primeira_dist']==ano_escolhido]
 
-    ajustes_dados = dados_varas[dados_varas['ano_primeira_dist'] == ano_escolhido]
-    ajustes_dados = ajustes_dados[ajustes_dados['Órgão Julgador'] == vara_selecionada]
+    vara_escolhida.sort_values(['mes_primeira_dist'])
 
-    top_janeiro = ajustes_dados[ajustes_dados['mes_primeira_dist']== 1]
-    top_5_janeiro = top_janeiro['Assunto'].value_counts()[:5].index.tolist()
-
-    top_fevereiro = ajustes_dados[ajustes_dados['mes_primeira_dist']== 2]
-    top_5_fevereiro = top_fevereiro['Assunto'].value_counts()[:5].index.tolist()
-
-    top_marco = ajustes_dados[ajustes_dados['mes_primeira_dist']== 3]
-    top_5_marco = top_marco['Assunto'].value_counts()[:5].index.tolist()
-
-    top_abril = ajustes_dados[ajustes_dados['mes_primeira_dist']== 4]
-    top_5_abril = top_abril['Assunto'].value_counts()[:5].index.tolist()
-
-    top_maio = ajustes_dados[ajustes_dados['mes_primeira_dist']== 5]
-    top_5_maio = top_maio['Assunto'].value_counts()[:5].index.tolist()
-
-    top_junho = ajustes_dados[ajustes_dados['mes_primeira_dist']== 6]
-    top_5_junho = top_junho['Assunto'].value_counts()[:5].index.tolist()
-
-    top_julho = ajustes_dados[ajustes_dados['mes_primeira_dist']== 7]
-    top_5_julho = top_julho['Assunto'].value_counts()[:5].index.tolist()
-
-    top_agosto = ajustes_dados[ajustes_dados['mes_primeira_dist']== 8]
-    top_5_agosto = top_agosto['Assunto'].value_counts()[:5].index.tolist()
-
-    top_setembro = ajustes_dados[ajustes_dados['mes_primeira_dist']== 9]
-    top_5_setembro = top_setembro['Assunto'].value_counts()[:5].index.tolist()
-
-    top_outubro = ajustes_dados[ajustes_dados['mes_primeira_dist']== 10]
-    top_5_outubro = top_outubro['Assunto'].value_counts()[:5].index.tolist()
-
-    top_novembro = ajustes_dados[ajustes_dados['mes_primeira_dist']== 11]
-    top_5_novembro = top_novembro['Assunto'].value_counts()[:5].index.tolist()
-
-    top_dezembro = ajustes_dados[ajustes_dados['mes_primeira_dist']== 12]
-    top_5_dezembro = top_dezembro['Assunto'].value_counts()[:5].index.tolist()
-
-    dados_top5_vara_df = {'Meses':['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
-                       'Assuntos mais frequentes':[top_5_janeiro,top_5_fevereiro,top_5_marco,top_5_abril,top_5_maio,top_5_junho,top_5_julho,top_5_agosto,top_5_setembro,top_5_outubro,top_5_novembro,top_5_dezembro]}
-    dados_top5_vara = pd.DataFrame(dados_top5_vara_df)
+    vara_escolhida['mes_primeira_dist'] = vara_escolhida['mes_primeira_dist'].replace(1,'01 - Jan')
+    vara_escolhida['mes_primeira_dist'] = vara_escolhida['mes_primeira_dist'].replace(2,'02 - Fev')
+    vara_escolhida['mes_primeira_dist'] = vara_escolhida['mes_primeira_dist'].replace(3,'03 - Mar')
+    vara_escolhida['mes_primeira_dist'] = vara_escolhida['mes_primeira_dist'].replace(4,'04 - Abr')
+    vara_escolhida['mes_primeira_dist'] = vara_escolhida['mes_primeira_dist'].replace(5,'05 - Mai')
+    vara_escolhida['mes_primeira_dist'] = vara_escolhida['mes_primeira_dist'].replace(6,'06 - Jun')
+    vara_escolhida['mes_primeira_dist'] = vara_escolhida['mes_primeira_dist'].replace(7,'07 - Jul')
+    vara_escolhida['mes_primeira_dist'] = vara_escolhida['mes_primeira_dist'].replace(8,'08 - Ago')
+    vara_escolhida['mes_primeira_dist'] = vara_escolhida['mes_primeira_dist'].replace(9,'09 - Set')
+    vara_escolhida['mes_primeira_dist'] = vara_escolhida['mes_primeira_dist'].replace(10,'10 - Out')
+    vara_escolhida['mes_primeira_dist'] = vara_escolhida['mes_primeira_dist'].replace(11,'11 - Nov')
+    vara_escolhida['mes_primeira_dist'] = vara_escolhida['mes_primeira_dist'].replace(12,'12 - Dez')
 
     #vara_escolhida['Assunto'] = vara['Assunto'].value_counts()[:5].index.tolist()
-    #pd.DataFrame({'a': pd.Series(a), 'b': pd.Series(b)})
 
-    fig = px.scatter(dados_top5_vara, x='Meses', y='Assuntos mais frequentes',
-             labels={'pop':'population of Canada'}, height=400)
-    fig.update_layout(barmode='stack')
+    traces = []
 
-    return fig
+    for assunto_processo in vara_escolhida['Assunto'].unique():
+        vara_filtrada = vara_escolhida[vara_escolhida['Assunto']==assunto_processo]
+        traces.append(go.Bar(
+               x=vara_filtrada['mes_primeira_dist'],
+               y=vara_filtrada['Assunto'],
+               showlegend=False
+         ))
+
+    return {'data':traces,
+            'layout':go.Layout(title= {"text": "Distribuição de processos por mês da {} no ano {}".format(vara_selecionada,ano_escolhido)},
+                               xaxis = {'title':'Mês da distribuição','categoryorder':'category ascending'},
+                               yaxis = {'title':'Assunto','visible':False},
+                               barmode='stack')}
 
 @app.callback(Output('table1','children'),
              [Input('escolhe-ano','value')])
@@ -416,6 +412,166 @@ def update_datatable(ano_escolhido, vara_escolhida):
     return(px.scatter_mapbox(dataframe_varas, lat="Latitude", lon="Longitude", color="Congestionamento",size="Congestionamento",
                      color_continuous_scale="Bluered", size_max=15, zoom=7,
                      mapbox_style="carto-positron", text="Órgão Julgador"))
+
+
+
+@app.callback(Output('table3','children'),
+            [Input('escolhe-ano','value'),
+            Input('escolhe-vara','value')])
+def update_datatable(ano_escolhido, vara_escolhida):
+
+    dados_analise = dados_varas[dados_varas['Órgão Julgador'] == vara_escolhida]
+    dados_analise = dados_analise[dados_analise['ano_primeira_dist'] == ano_escolhido]
+
+    dados_filtrados = dados_analise.groupby(['mes_primeira_dist', 'Assunto'])['Assunto Código'].count()
+    dados_ajustados = dados_filtrados.groupby(level='mes_primeira_dist').nlargest(25).reset_index(level=0, drop=True).reset_index()
+
+    estatisticas_ano = dados_ajustados["Assunto Código"].describe()
+
+    media_ano = estatisticas_ano[1]
+    desvio_padrao_ano = estatisticas_ano[2]
+    anomalia_ano = 2*desvio_padrao_ano + media_ano
+
+    # janeiro
+    dados_janeiro = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 1]
+    #janeiro = dados_janeiro["Assunto Código"].describe()
+    # isso abaixo é um DataFrame
+    janeiro_acima_media = dados_janeiro[dados_janeiro["Assunto Código"] > media_ano]
+    janeiro_anomalia = dados_janeiro[dados_janeiro["Assunto Código"] > anomalia_ano]
+    # encontrando os assuntos de janeiro
+    janeiro_acima_media_lista = janeiro_acima_media["Assunto"].tolist()
+    janeiro_anomalia_lista = janeiro_anomalia["Assunto"].tolist()
+
+    # fevereiro
+    dados_fevereiro = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 2]
+    fevereiro_acima_media = dados_fevereiro[dados_fevereiro["Assunto Código"] > media_ano]
+    fevereiro_anomalia = dados_fevereiro[dados_fevereiro["Assunto Código"] > anomalia_ano]
+    # encontrando os assuntos de fevereiro
+    fevereiro_acima_media_lista = fevereiro_anomalia_acima_media["Assunto"].tolist()
+    fevereiro_anomalia_lista = fevereiro_anomalia["Assunto"].tolist()
+
+    # março
+    dados_marco = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 3]
+    marco_acima_media = dados_marco[dados_marco["Assunto Código"] > media_ano]
+    marco_anomalia = dados_marco[dados_marco["Assunto Código"] > anomalia_ano]
+    # encontrando os assuntos de fevereiro
+    marco_acima_media_lista = marco_acima_media["Assunto"].tolist()
+    marco_anomalia_lista = marco_anomalia["Assunto"].tolist()
+
+    # abril
+    dados_abril = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 4]
+    abril_acima_media = dados_abril[dados_abril["Assunto Código"] > media_ano]
+    abril_anomalia = dados_abril[dados_abril["Assunto Código"] > anomalia_ano]
+    # encontrando os assuntos de fevereiro
+    abril_acima_media_lista = abril_acima_media["Assunto"].tolist()
+    abril_anomalia_lista = abril_anomalia["Assunto"].tolist()
+
+    # maio
+    dados_maio = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 5]
+    maio_acima_media = dados_maio[dados_maio["Assunto Código"] > media_ano]
+    maio_anomalia = dados_maio[dados_maio["Assunto Código"] > anomalia_ano]
+    # encontrando os assuntos de fevereiro
+    maio_acima_media_lista = maio_acima_media["Assunto"].tolist()
+    maio_anomalia_lista = maio_anomalia["Assunto"].tolist()
+
+    # junho
+    dados_junho = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 6]
+    junho_acima_media = dados_junho[dados_junho["Assunto Código"] > media_ano]
+    junho_anomalia = dados_junho[dados_junho["Assunto Código"] > anomalia_ano]
+    # encontrando os assuntos de fevereiro
+    junho_acima_media_lista = junho_acima_media["Assunto"].tolist()
+    junho_anomalia_lista = junho_anomalia["Assunto"].tolist()
+
+    # julho
+    dados_julho = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 7]
+    julho_acima_media = dados_julho[dados_julho["Assunto Código"] > media_ano]
+    julho_anomalia = dados_julho[dados_julho["Assunto Código"] > anomalia_ano]
+    # encontrando os assuntos de fevereiro
+    julho_acima_media_lista = julho_acima_media["Assunto"].tolist()
+    julho_anomalia_lista = julho_anomalia["Assunto"].tolist()
+
+    # agosto
+    dados_agosto = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 8]
+    agosto_acima_media = dados_agosto[dados_agosto["Assunto Código"] > media_ano]
+    agosto_anomalia = dados_agosto[dados_agosto["Assunto Código"] > anomalia_ano]
+    # encontrando os assuntos de fevereiro
+    agosto_acima_media_lista = agosto_acima_media["Assunto"].tolist()
+    agosto_anomalia_lista = agosto_anomalia["Assunto"].tolist()
+
+    # setembro
+    dados_setembro = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 9]
+    setembro_acima_media = dados_setembro[dados_setembro["Assunto Código"] > media_ano]
+    setembro_anomalia = dados_setembro[dados_setembro["Assunto Código"] > anomalia_ano]
+    # encontrando os assuntos de fevereiro
+    setembro_acima_media_lista = setembro_acima_media["Assunto"].tolist()
+    setembro_anomalia_lista = setembro_anomalia["Assunto"].tolist()
+
+    # outubro
+    dados_outubro = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 10]
+    outubro_acima_media = dados_outubro[dados_outubro["Assunto Código"] > media_ano]
+    outubro_anomalia = dados_outubro[dados_outubro["Assunto Código"] > anomalia_ano]
+    # encontrando os assuntos de fevereiro
+    outubro_acima_media_lista = outubro_acima_media["Assunto"].tolist()
+    outubro_anomalia_lista = outubro_anomalia["Assunto"].tolist()
+
+    # novembro
+    dados_novembro = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 11]
+    novembro_acima_media = dados_novembro[dados_novembro["Assunto Código"] > media_ano]
+    novembro_anomalia = dados_novembro[dados_novembro["Assunto Código"] > anomalia_ano]
+    # encontrando os assuntos de fevereiro
+    novembro_acima_media_lista = novembro_acima_media["Assunto"].tolist()
+    novembro_anomalia_lista = novembro_anomalia["Assunto"].tolist()
+
+    # dezembro
+    dados_dezembro = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 12]
+    dezembro_acima_media = dados_dezembro[dados_dezembro["Assunto Código"] > media_ano]
+    dezembro_anomalia = dados_dezembro[dados_dezembro["Assunto Código"] > anomalia_ano]
+    # encontrando os assuntos de fevereiro
+    dezembro_acima_media_lista = dezembro_acima_media["Assunto"].tolist()
+    dezembro_anomalia_lista = dezembro_anomalia["Assunto"].tolist()
+
+    dados_analise_estatistica = pd.DataFrame({
+
+        "Janeiro - Acima da média": janeiro_acima_media_lista,
+        "Janeiro - Muito acima da média": janeiro_anomalia_lista,
+        "Fevereiro - Acima da média": fevereiro_acima_media_lista,
+        "Fevereiro - Muito acima da média": fevereiro_anomalia_lista,
+        "Março - Acima da média": marco_acima_media_lista,
+        "Março - Muito acima da média": marco_anomalia_lista,
+        "Abril - Acima da média": abril_acima_media_lista,
+        "Abril - Muito acima da média": abril_anomalia_lista,
+        "Maio - Acima da média": maio_acima_media_lista,
+        "Maio - Muito acima da média": maio_anomalia_lista,
+        "Junho - Acima da média": junho_acima_media_lista,
+        "Junho - Muito acima da média": junho_anomalia_lista,
+        "Julho - Acima da média": julho_acima_media_lista,
+        "Julho - Muito acima da média": julho_anomalia_lista,
+        "Agosto - Acima da média": agosto_acima_media_lista,
+        "Agosto - Muito acima da média": agosto_anomalia_lista,
+        "Setembro - Acima da média": setembro_acima_media_lista,
+        "Setembro - Muito acima da média": setembro_anomalia_lista,
+        "Outubro - Acima da média": outubro_acima_media_lista,
+        "Outubro - Muito acima da média": outubro_anomalia_lista,
+        "Novembro - Acima da média": novembro_acima_media_lista,
+        "Novembro - Muito acima da média": novembro_anomalia_lista,
+        "Dezembro - Acima da média": dezembro_acima_media_lista,
+        "Dezembro - Muito acima da média": dezembro_anomalia_lista,
+    })
+
+
+    estilo_celula = [{
+            'if': {'row_index': 'odd'},
+            'backgroundColor': 'rgb(248, 248, 248)'
+    }]
+    estilo_cabecalho = {
+        'backgroundColor': 'rgb(230, 230, 230)',
+        'fontWeight': 'bold'
+    }
+    data = dados_analise_estatistica.to_dict('rows')
+    columns =  [{"name": i, "id": i,} for i in (dados_analise_estatistica.columns)]
+    return (html.H3('Tabela com análise de Assuntos que entraram na {} no ano {}:'.format(vara_escolhida,ano_escolhido), style=TEXT_STYLE),
+            dt.DataTable(data=data, columns=columns,style_table={'overflowX': 'auto'},style_data_conditional=estilo_celula,style_header = estilo_cabecalho))
+
 
 
 if __name__ == '__main__':
--- 01 - Prototipo/ajuste_dados_3.py
+++ 01 - Prototipo/ajuste_dados_3.py
@@ -42,15 +42,104 @@ anomalia_ano = 2*desvio_padrao_ano + media_ano
 
 print("A média de processos por assunto no ano {} na {} foi de {}, uma anomalia é considerada se houverem mais que {} processos de determinado Assunto".format(ano, vara, round(media_ano,2), round(anomalia_ano,2)))
 
+# janeiro
 dados_janeiro = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 1]
 #janeiro = dados_janeiro["Assunto Código"].describe()
-
 # isso abaixo é um DataFrame
 janeiro_acima_media = dados_janeiro[dados_janeiro["Assunto Código"] > media_ano]
 janeiro_anomalia = dados_janeiro[dados_janeiro["Assunto Código"] > anomalia_ano]
-
+# encontrando os assuntos de janeiro
 janeiro_acima_media_lista = janeiro_acima_media["Assunto"].tolist()
 janeiro_anomalia_lista = janeiro_anomalia["Assunto"].tolist()
+
+# fevereiro
+dados_fevereiro = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 2]
+fevereiro_acima_media = dados_fevereiro[dados_fevereiro["Assunto Código"] > media_ano]
+fevereiro_anomalia = dados_fevereiro[dados_fevereiro["Assunto Código"] > anomalia_ano]
+# encontrando os assuntos de fevereiro
+fevereiro_acima_media_lista = fevereiro_anomalia_acima_media["Assunto"].tolist()
+fevereiro_anomalia_lista = fevereiro_anomalia["Assunto"].tolist()
+
+# março
+dados_marco = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 3]
+marco_acima_media = dados_marco[dados_marco["Assunto Código"] > media_ano]
+marco_anomalia = dados_marco[dados_marco["Assunto Código"] > anomalia_ano]
+# encontrando os assuntos de fevereiro
+marco_acima_media_lista = marco_acima_media["Assunto"].tolist()
+marco_anomalia_lista = marco_anomalia["Assunto"].tolist()
+
+# abril
+dados_abril = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 4]
+abril_acima_media = dados_abril[dados_abril["Assunto Código"] > media_ano]
+abril_anomalia = dados_abril[dados_abril["Assunto Código"] > anomalia_ano]
+# encontrando os assuntos de fevereiro
+abril_acima_media_lista = abril_acima_media["Assunto"].tolist()
+abril_anomalia_lista = abril_anomalia["Assunto"].tolist()
+
+# maio
+dados_maio = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 5]
+maio_acima_media = dados_maio[dados_maio["Assunto Código"] > media_ano]
+maio_anomalia = dados_maio[dados_maio["Assunto Código"] > anomalia_ano]
+# encontrando os assuntos de fevereiro
+maio_acima_media_lista = maio_acima_media["Assunto"].tolist()
+maio_anomalia_lista = maio_anomalia["Assunto"].tolist()
+
+# junho
+dados_junho = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 6]
+junho_acima_media = dados_junho[dados_junho["Assunto Código"] > media_ano]
+junho_anomalia = dados_junho[dados_junho["Assunto Código"] > anomalia_ano]
+# encontrando os assuntos de fevereiro
+junho_acima_media_lista = junho_acima_media["Assunto"].tolist()
+junho_anomalia_lista = junho_anomalia["Assunto"].tolist()
+
+# julho
+dados_julho = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 7]
+julho_acima_media = dados_julho[dados_julho["Assunto Código"] > media_ano]
+julho_anomalia = dados_julho[dados_julho["Assunto Código"] > anomalia_ano]
+# encontrando os assuntos de fevereiro
+julho_acima_media_lista = julho_acima_media["Assunto"].tolist()
+julho_anomalia_lista = julho_anomalia["Assunto"].tolist()
+
+# agosto
+dados_agosto = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 8]
+agosto_acima_media = dados_agosto[dados_agosto["Assunto Código"] > media_ano]
+agosto_anomalia = dados_agosto[dados_agosto["Assunto Código"] > anomalia_ano]
+# encontrando os assuntos de fevereiro
+agosto_acima_media_lista = agosto_acima_media["Assunto"].tolist()
+agosto_anomalia_lista = agosto_anomalia["Assunto"].tolist()
+
+# setembro
+dados_setembro = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 9]
+setembro_acima_media = dados_setembro[dados_setembro["Assunto Código"] > media_ano]
+setembro_anomalia = dados_setembro[dados_setembro["Assunto Código"] > anomalia_ano]
+# encontrando os assuntos de fevereiro
+setembro_acima_media_lista = setembro_acima_media["Assunto"].tolist()
+setembro_anomalia_lista = setembro_anomalia["Assunto"].tolist()
+
+# outubro
+dados_outubro = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 10]
+outubro_acima_media = dados_outubro[dados_outubro["Assunto Código"] > media_ano]
+outubro_anomalia = dados_outubro[dados_outubro["Assunto Código"] > anomalia_ano]
+# encontrando os assuntos de fevereiro
+outubro_acima_media_lista = outubro_acima_media["Assunto"].tolist()
+outubro_anomalia_lista = outubro_anomalia["Assunto"].tolist()
+
+# novembro
+dados_novembro = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 11]
+novembro_acima_media = dados_novembro[dados_novembro["Assunto Código"] > media_ano]
+novembro_anomalia = dados_novembro[dados_novembro["Assunto Código"] > anomalia_ano]
+# encontrando os assuntos de fevereiro
+novembro_acima_media_lista = novembro_acima_media["Assunto"].tolist()
+novembro_anomalia_lista = novembro_anomalia["Assunto"].tolist()
+
+# dezembro
+dados_dezembro = dados_ajustados[dados_ajustados['mes_primeira_dist'] == 12]
+dezembro_acima_media = dados_dezembro[dados_dezembro["Assunto Código"] > media_ano]
+dezembro_anomalia = dados_dezembro[dados_dezembro["Assunto Código"] > anomalia_ano]
+# encontrando os assuntos de fevereiro
+dezembro_acima_media_lista = dezembro_acima_media["Assunto"].tolist()
+dezembro_anomalia_lista = dezembro_anomalia["Assunto"].tolist()
+
 
 '''
 #['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro']
@@ -59,7 +148,7 @@ prototipo de dataframe para mostrar em gráfico de barras
 df = {'Meses':['Janeiro'],
       'Anomalia':janeiro_anomalia_lista,
       'Média':janeiro_acima_media_lista}
-'''
+
 
 print("A seguir serão mostrados os Assuntos que apareceram em Janeiro numa frequência acima da média: ")
 for i in range(len(janeiro_acima_media_lista)):
@@ -72,3 +161,4 @@ for j in range(len(janeiro_anomalia_lista)):
 df = {'Meses':['Janeiro'],
       'Anomalia':janeiro_anomalia_lista,
       'Média':janeiro_acima_media_lista}
+'''
